702d2fcf537033d8fb73cc42d2ea1cd4d117e632
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-03-28 11:44:20 +0200

    Problem: no CI for cppzmq
    Solution: add travis-ci

---
+++ ci_build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -x
+
+install_zeromq() {
+    pushd .
+
+    mkdir libzmq
+    cd libzmq
+    curl -L https://github.com/zeromq/libzmq/releases/download/v${ZMQ_VERSION}/zeromq-${ZMQ_VERSION}.tar.gz >zeromq.tar.gz
+    tar -xvzf zeromq.tar.gz
+    cd zeromq-${ZMQ_VERSION}
+
+    mkdir build
+    cd build
+    cmake ..
+    sudo make -j4 install
+
+    popd
+}
+
+# build zeromq first
+
+if [ "${ZMQ_VERSION}" != "" ] ; then install_zeromq ; fi
+
+# build cppzmq
+
+mkdir build
+cd build
+cmake ..
+sudo make -j4 install
+
+# build cppzmq tests
+# cd tests
+# mkdir build
+# cd build
+# cmake ..
+# make -j5 test ARGS="-V"
