b737b9f1dee1b49c7e6d6e6057ce3703726e4b54
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-04-02 13:15:34 +0200

    Problem: no test cases
    Solution: added a few initial test cases

--- tests/example_add.cpp
+++ tests/example_add.cpp
@@ -1,9 +1,21 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include <zmq.hpp>
 
-TEST(create_context, add)
+TEST(context, create_default_destroy)
 {
     zmq::context_t context;
+}
+
+TEST(context, create_close)
+{
+    zmq::context_t context;
+    context.close();
     
-    
+    ASSERT_EQ(NULL, (void*)context);
+}
+
+TEST(socket, create_destroy)
+{
+    zmq::context_t context;
+    zmq::socket_t socket(context, ZMQ_ROUTER);
 }
