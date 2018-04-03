83f854869aebdeb4aef9f4d73fe5f329e94f634c
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-04-03 12:04:23 +0200

    Problem: no tests for socket_t ctor accepting enum socket_type
    Solution: added test case

--- tests/socket.cpp
+++ tests/socket.cpp
@@ -7,3 +7,10 @@ TEST(socket, create_destroy)
     zmq::socket_t socket(context, ZMQ_ROUTER);
 }
 
+#ifdef ZMQ_CPP11
+TEST(socket, create_by_enum_destroy)
+{
+    zmq::context_t context;
+    zmq::socket_t socket(context, zmq::socket_type::router);
+}
+#endif
